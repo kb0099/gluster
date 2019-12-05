128ad2a42764f458817ddf342d641e2ba04a2283
KB <kedar_bas@hotmail.com>
2019-12-05 14:07:29 -0600

    changes

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-declare d1="a";
-declare d2="b";
-declare d3="c";
+declare d1="g1";
+declare d2="g2";
+declare d3="g3";
 
 sudo rm -rf ./appdir
 # Some files don't want to get deleted -> move them
@@ -13,9 +13,9 @@ echo "\nOld files cleared."
 echo "\nIniting"
 python ./init.py
 
-mv ../gluster/appdir/systems/gl/g1 ../gluster/appdir/systems/gl/$d1
-mv ../gluster/appdir/systems/gl/g2 ../gluster/appdir/systems/gl/$d2
-mv ../gluster/appdir/systems/gl/g3 ../gluster/appdir/systems/gl/$d3
+# mv ../gluster/appdir/systems/gl/g1 ../gluster/appdir/systems/gl/$d1
+# mv ../gluster/appdir/systems/gl/g2 ../gluster/appdir/systems/gl/$d2
+# mv ../gluster/appdir/systems/gl/g3 ../gluster/appdir/systems/gl/$d3
 
 docker rm $(docker stop -t 0 $(docker ps -aq))
 
